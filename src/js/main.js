9c2c17edd3ba9220e18d2fc577fad3054327da8f
Gibran Castillo <gibran-castillo@hotmail.com>
2022-04-30 15:10:06 -0500

    Convert hardcoded tents product list to a dynamic and generic product list

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
-import ProductData from './productData.js';
-import ProductList from './ProductList.js';
-const dataSource = new ProductList('lists');
-console.log(dataSource.getData());
+import ProductData from "./productData.js";
+import ProductList from "./productList.js";
+
+// Create an instance of our ProductData class.
+const dataSource = new ProductData("tents");
+// console.log(dataSource.getData());
+
+// Get the element we want the product list to render in
+const listElement = document.querySelector(".product-list");
+
+// Create an instance of our ProductList class and send it the correct information.
+const myProductList = new ProductList("tents", dataSource, listElement);
+
+// Call the init method to show our products
+myProductList.init();
