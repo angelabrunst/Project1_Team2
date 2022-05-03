a81efe007ad1b054ab455c54dc6ba392e28fae2f
Gibran Castillo <gibran-castillo@hotmail.com>
2022-05-02 22:11:14 -0500

    Add all categories and dynamically generate each category product list and the product details page for each category

--- build/js/product.js
+++ build/js/product.js
@@ -1,1 +1,7 @@
-import t from"./productData.js";import o from"./productDetails.js";import{getParam as r}from"./utils.js";const c=new t,a=r("product"),d=new o(a,c);d.init();
+import t from "./productData.js";
+import o from "./productDetails.js";
+import { getParam as r } from "./utils.js";
+const c = new t(),
+  a = r("product"),
+  d = new o(a, c);
+d.init();
